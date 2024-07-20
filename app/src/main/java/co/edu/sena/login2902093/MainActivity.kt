8c296aba1b04132f85f8f17ae7592c6446a63b21
VaNe7777 <guerrerolindavanessa@gmail.com>
2024-07-20 18:52:27 -0500

    feat: commit inicial

---
+++ app/src/main/java/co/edu/sena/login2902093/MainActivity.kt
@@ -0,0 +1,42 @@
+package co.edu.sena.login2902093
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import co.edu.sena.login2902093.app.PostOfficeApp
+import co.edu.sena.login2902093.ui.theme.Login2902093Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PostOfficeApp()
+        }
+    } // onCreate
+} // MainActivity
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Login2902093Theme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/co/edu/sena/login2902093/screens/SignUpScreen.kt
@@ -0,0 +1,22 @@
+package co.edu.sena.login2902093.screens
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun SignUpScreen() {
+    Surface(color = Color.White,
+            modifier = Modifier.fillMaxSize()) {
+
+    }
+}
+
+@Preview
+@Composable
+
+fun Default
