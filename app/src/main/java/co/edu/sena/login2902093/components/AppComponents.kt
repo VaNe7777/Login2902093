d33488c1fe8a55b257e264daef63b6dda0c819ba
VaNe7777 <guerrerolindavanessa@gmail.com>
2024-07-21 17:32:51 -0500

    feat: commit inicial

--- app/src/main/java/co/edu/sena/login2902093/components/AppComponents.kt
+++ app/src/main/java/co/edu/sena/login2902093/components/AppComponents.kt
@@ -3,24 +3,33 @@ package co.edu.sena.login2902093.components
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import co.edu.sena.login2902093.R
+import co.edu.sena.login2902093.ui.theme.TextColor
+
 
 @Composable
 
 fun NormalTextComponent(values:String){
     Text(
         text = values,
-        modifier = Modifier.fillMaxWidth().heightIn(min = 80.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .heightIn(min = 80.dp),
         style = TextStyle(
             fontSize = 18.sp,
             fontWeight = FontWeight.Normal,
             fontStyle = FontStyle.Normal
         )
+
+        , color = colorResource(id = R.color.colorText)
     )
 }
--- app/src/main/java/co/edu/sena/login2902093/screens/SignUpScreen.kt
+++ app/src/main/java/co/edu/sena/login2902093/screens/SignUpScreen.kt
@@ -1,26 +1,42 @@
 package co.edu.sena.login2902093.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import co.edu.sena.login2902093.R
+import co.edu.sena.login2902093.components.NormalTextComponent
 
 
 @Composable
 fun SignUpScreen() {
-    Surface(color = Color.White,
-            modifier = Modifier.fillMaxSize()
+    Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White)
+                .padding(28.dp)
     ) {
-
+        NormalTextComponent(values = stringResource(id = R.string.hello))
     }
 }
+
 
 @Preview
 @Composable
 
 fun DefaultPreviewSingUpScreen(){
+    Surface(
+        color = Color.White,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        NormalTextComponent(values = stringResource(id = R.string.hello))
+    }
 }
 
 
