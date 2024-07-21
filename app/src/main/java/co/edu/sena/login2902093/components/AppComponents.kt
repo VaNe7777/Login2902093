c32648df947866258130627a1679ffe7ad55bdf6
VaNe7777 <guerrerolindavanessa@gmail.com>
2024-07-21 17:57:46 -0500

    feat: commit inicial

--- app/src/main/java/co/edu/sena/login2902093/components/AppComponents.kt
+++ app/src/main/java/co/edu/sena/login2902093/components/AppComponents.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import co.edu.sena.login2902093.R
@@ -30,6 +31,26 @@ fun NormalTextComponent(values:String){
             fontStyle = FontStyle.Normal
         )
 
-        , color = colorResource(id = R.color.colorText)
+        , color = colorResource(id = R.color.colorText),
+        textAlign = TextAlign.Center
     )
 }
+@Composable
+
+fun HeadingTextComponent(values:String){
+    Text(
+        text = values,
+        modifier = Modifier
+            .fillMaxWidth()
+            .heightIn(min = 80.dp),
+        style = TextStyle(
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Normal
+        )
+
+        , color = colorResource(id = R.color.colorText),
+        textAlign = TextAlign.Center
+    )
+}
+
